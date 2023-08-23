dbbc00510e6a86c996edc69f2926cff9efa469c2
chrislitw <chrisli.dev@gmail.com>
2023-08-23 09:39:31 +0800

    fix: update chat layout

--- client/src/components/MessageBox.vue
+++ client/src/components/MessageBox.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="max-w-fit rounded-lg bg-red-200 px-3 py-1"><slot></slot></div>
+  <div class="w-fit max-w-[200px] break-words rounded bg-blue-500 px-3 py-1 text-white">
+    <span> <slot></slot> </span>
+  </div>
 </template>
 <style scoped></style>
